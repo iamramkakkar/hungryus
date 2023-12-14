a7e465d612279e70b521ead2d991b23116787a9b
iamramkakkar <33386993+iamramkakkar@users.noreply.github.com>
2023-12-14 22:49:47 +0000

    Pending changes exported from your codespace

--- Hello.py
+++ Hello.py
@@ -14,38 +14,116 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+import pandas as pd
+import time
+import os 
+import datetime as dt
 
 LOGGER = get_logger(__name__)
 
 
 def run():
+    
     st.set_page_config(
         page_title="Hello",
         page_icon="👋",
     )
 
     st.write("# Welcome to Streamlit! 👋")
+    
 
-    st.sidebar.success("Select a demo above.")
+    # df = pd.DataFrame({
+    #     'first column': [1, 2, 3, 4],
+    #     'second column': [10, 20, 30, 40]
+    #     })
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    # option = st.selectbox(
+    #     'Which number do you like best?',
+    #     df['first column'])
 
+    # 'You selected: ', option
+    # #st.sidebar.success("Select a demo above.")
+    # # Add a selectbox to the sidebar:
+    # add_selectbox = st.sidebar.selectbox(
+    #     'How would you like to be contacted?',
+    #     ('Email', 'Home phone', 'Mobile phone')
+    # )
+
+    # # Add a slider to the sidebar:
+    # add_slider = st.sidebar.slider(
+    #     'Select a range of values',
+    #     0.0, 100.0, (25.0, 75.0)
+        
+    # )
+
+    # 'Starting a long computation...'
+
+    # # # Add a placeholder
+    # # latest_iteration = st.empty()
+    # # bar = st.progress(0)
+
+    # # for i in range(100):
+    # #   # Update the progress bar with each iteration.
+    # #   latest_iteration.text(f'Iteration {i+1}')
+    # #   bar.progress(i + 1)
+    # #   time.sleep(0.1)
+
+
+
+    l0 = os.listdir("data/")
+    # st.write(os.listdir("data/"))
+
+    df = pd.DataFrame(l0,columns =["FileName"] )
+    # st.dataframe(df)
+
+    df['Date1'] = df['FileName'].str.split("__").str[1]
+    df['Year'] = df['Date1'].str[:4]
+    df['Month'] = df['Date1'].str[4:6]
+    df['Day'] = df['Date1'].str[6:8]
+    df['Date'] = pd.to_datetime(df[['Year', 'Month', 'Day']]).dt.date
+    df.drop(['Date1', 'Year', 'Month', 'Day'],axis=1,inplace=True)
+    df.sort_values(by='Date',ascending=False, inplace=True, ignore_index=True)
+    # st.dataframe(df)
+
+    range_selected = st.slider("Select a range of date",
+              dt.date(2019, 7, 6),
+              dt.date(2024, 12, 12),
+              (dt.date(2022, 7, 6),dt.date(2022, 12, 12)))
+
+    st.markdown(range_selected[0])
+    st.markdown(range_selected[1])
+    
+    col1, col2, col3 = st.columns(3)
+
+    df2 = df.loc[(df['Date']>range_selected[0]) & (df['Date']<range_selected[1])]
+    l_filenames = list(df2['FileName'])
+    for i in range(0,len(l_filenames),3):
+        try:
+            with col1:
+                st.image("data/"+l_filenames[i])
+            with col2:
+                st.image("data/"+l_filenames[i+1])
+            with col3:
+                st.image("data/"+l_filenames[i+2])
+        except Exception as e:
+            print()
+
+
+    
+    
+
+    # with col1:
+    #     st.header("A cat")
+    #     st.image("https://static.streamlit.io/examples/cat.jpg")
+
+    # with col2:
+    #     st.header("A dog")
+    #     st.image("https://static.streamlit.io/examples/dog.jpg")
+
+    # with col3:
+    #     st.header("An owl")
+    #     st.image("https://static.streamlit.io/examples/owl.jpg")
+    #     st.image("data/pizza.jpeg", caption="Dominos Pizza in Jaipur")
 
 if __name__ == "__main__":
     run()
