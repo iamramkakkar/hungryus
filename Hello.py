3a5d38c9d1fdbfad7e749c4872b98d5428e37fb4
iamramkakkar <33386993+iamramkakkar@users.noreply.github.com>
2023-12-14 18:23:21 +0000

    Pending changes exported from your codespace

--- Hello.py
+++ Hello.py
@@ -14,20 +14,62 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+import pandas as pd
+import time
 
 LOGGER = get_logger(__name__)
 
 
 def run():
+    
     st.set_page_config(
         page_title="Hello",
         page_icon="👋",
     )
 
     st.write("# Welcome to Streamlit! 👋")
+    
 
-    st.sidebar.success("Select a demo above.")
+    df = pd.DataFrame({
+        'first column': [1, 2, 3, 4],
+        'second column': [10, 20, 30, 40]
+        })
 
+    option = st.selectbox(
+        'Which number do you like best?',
+        df['first column'])
+
+    'You selected: ', option
+    #st.sidebar.success("Select a demo above.")
+    # Add a selectbox to the sidebar:
+    add_selectbox = st.sidebar.selectbox(
+        'How would you like to be contacted?',
+        ('Email', 'Home phone', 'Mobile phone')
+    )
+
+    # Add a slider to the sidebar:
+    add_slider = st.sidebar.slider(
+        'Select a range of values',
+        0.0, 100.0, (25.0, 75.0)
+        
+    )
+
+
+    
+
+    'Starting a long computation...'
+
+    # # Add a placeholder
+    # latest_iteration = st.empty()
+    # bar = st.progress(0)
+
+    # for i in range(100):
+    #   # Update the progress bar with each iteration.
+    #   latest_iteration.text(f'Iteration {i+1}')
+    #   bar.progress(i + 1)
+    #   time.sleep(0.1)
+
+    # '...and now we\'re done!'
     st.markdown(
         """
         Streamlit is an open-source app framework built specifically for
@@ -45,7 +87,7 @@ def run():
         - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
     """
     )
-
+    st.image("data/pizza.jpeg")
 
 if __name__ == "__main__":
     run()
